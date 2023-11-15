8a119126b83c15f7d92212c8cc1d2d5a06c8ac43
PedroLorea <p.lorea@edu.pucrs.br>
2023-11-15 12:09:17 -0300

    feat() - Estrutura inicial

---
+++ Algorithms.java
@@ -0,0 +1,22 @@
+public class Algorithms implements IAlgorithms {
+    
+    
+    public void solveBreadthFirst(int initialMatrix[][], int finalMatrix[][], int x, int y){
+
+    }
+
+
+    public void solveDeapthFirst(int initialMatrix[][], int finalMatrix[][], int x, int y){
+
+    }
+
+
+    public void solveGreedyBestFirst(int initialMatrix[][], int finalMatrix[][], int x, int y){
+
+    }
+
+    
+    public void solveAStar(int initialMatrix[][], int finalMatrix[][], int x, int y){
+
+    }
+}
---
+++ IAlgorithms.java
@@ -0,0 +1,10 @@
+public interface IAlgorithms {
+    
+    void solveBreadthFirst(int initialMatrix[][], int finalMatrix[][], int x, int y);
+
+    void solveDeapthFirst(int initialMatrix[][], int finalMatrix[][], int x, int y);
+
+    void solveGreedyBestFirst(int initialMatrix[][], int finalMatrix[][], int x, int y);
+
+    void solveAStar(int initialMatrix[][], int finalMatrix[][], int x, int y);
+}
